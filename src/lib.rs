06006ea28909cfabc0da7d3159a8b1c6f645d0bc
Isaac Lee <iasacrlee@gmail.com>
2019-05-31 15:06:17 -0500

    Setup structs, errors, and skeleton code
    Co-authored-by: adonnell6 <donnell.andrew.j@gmail.com>
    Co-authored-by: Alex Grimes <alexkgrimes@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,306 @@
+use chrono::{DateTime, Duration, NaiveDate, NaiveTime, Utc};
+use regex::RegexSet;
+use std::fmt;
 
+#[derive(Debug)]
+pub enum DateParseError {
+    DateBadFormat,
+    IoError(std::io::Error),
+}
+
+impl fmt::Display for DateParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DateParseError::DateBadFormat => write!(f, "Error: Date not formatted correctly"),
+            DateParseError::IoError(e) => write!(f, "IO Error: {}", e),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum TimeParseError {
+    TimeBadFormat,
+    IoError(std::io::Error),
+}
+
+impl fmt::Display for TimeParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TimeParseError::TimeBadFormat => write!(f, "Error: Time not formatted correctly"),
+            TimeParseError::IoError(e) => write!(f, "IO Error: {}", e),
+        }
+    }
+}
+
+// Examples
+// (12pm, 12, noon, twelve, at 12, 10:30, 12:30pm}
+// {Saturday, 6/1, sat, this saturday, next saturday, last saturday, june 1, june 1st}
+// {tonight, last night, tomorrow night, tomorrow morning, lunch, dinner, breakfast, dawn, late, afternoon, evening, now, in two hours, midnight}
+
+// Parser
+
+// DateTime Parser
+
+/// Contains parsing options.
+// pub struct DateTimeParser {}
+
+// impl DateTimeParser {
+//     pub fn parse(&self, text: &str) -> Result<DateTime<Utc>, DateTimeParseError> {
+//         self.parse_relative(text, Some(&Utc::now()))
+//     }
+
+//     pub fn parse_relative(
+//         &self,
+//         text: &str,
+//         now: Option<&DateTime<Utc>>,
+//     ) -> Result<DateTime<Utc>, DateTimeParseError> {
+//         unimplemented!()
+//     }
+// }
+
+// Date Parser
+pub struct DateParser {}
+impl DateParser {
+    pub fn parse(&self, text: &str) -> Result<NaiveDate, DateParseError> {
+        self.parse_relative(text, Some(&Utc::now().date().naive_utc()))
+    }
+
+    pub fn parse_relative(
+        &self,
+        text: &str,
+        now: Option<&NaiveDate>,
+    ) -> Result<NaiveDate, DateParseError> {
+        unimplemented!()
+        // DateExpr.recognize(text)
+
+        // match DateExpr
+        // create Date based on DateExpr and now
+    }
+}
+
+// Time Parser
+pub struct TimeParser {}
+impl TimeParser {
+    pub fn parse(&self, text: &str) -> Result<NaiveTime, TimeParseError> {
+        self.parse_relative(text, Some(&Utc::now().time()))
+    }
+
+    pub fn parse_relative(
+        &self,
+        text: &str,
+        now: Option<&NaiveTime>,
+    ) -> Result<NaiveTime, TimeParseError> {
+        unimplemented!()
+        // TimeExpr.recognize(text)
+
+        // match TimeExpr
+        // create NaiveTime based on DateExpr and now
+    }
+}
+
+// Expressions
+#[derive(Debug, PartialEq)]
+/// 0 means 1 BC, -1 means 2 BC, etc.?
+struct Year(pub isize);
+
+#[derive(Debug, PartialEq)]
+enum MonthOfYear {
+    Jan,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec,
+}
+
+#[derive(Debug, PartialEq)]
+struct Month {
+    year: Year,
+    month: MonthOfYear,
+}
+
+#[derive(Debug, PartialEq)]
+enum DayOfWeek {
+    Sun,
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat,
+}
+
+#[derive(Debug, PartialEq)]
+enum YearExpr {
+    ThisYear,
+    Absolute(Year),
+    // OfMonth(Box<MonthExpr>),
+    // OfWeek(Box<WeekExpr>),
+    // OfDay(Box<DateExpr>),
+    // Since(Box<YearExpr>, Duration)
+    InNYears(usize),
+}
+
+#[derive(Debug, PartialEq)]
+enum MonthExpr {
+    ThisMonth,
+    Absolute(Month),
+    InYear(Box<YearExpr>, MonthOfYear),
+    // OfWeek(Box<WeekExpr>),
+    // OfDay(Box<DateExpr>),
+    // Since(Box<MonthExpr>, Duration),
+    // NthSince(Box<MonthExpr>, isize, MonthOfYear),
+    InNMonths(usize),
+}
+
+#[derive(Debug, PartialEq)]
+enum WeekExpr {
+    ThisWeek,
+    Absolute(Year, i8),
+    InMonth(Box<MonthExpr>, i8),
+    // InYear(Box<YearExpr>, i8),
+    // OfDay(Box<DateExpr>),
+    // Since(Box<WeekExpr>, Duration),
+    InNWeeks(usize),
+}
+
+#[derive(Debug, PartialEq)]
+enum DateExpr {
+    Today,
+    Absolute(NaiveDate),
+    InWeek(Box<WeekExpr>, DayOfWeek),
+    // InMonth(Box<MonthExpr>, i8),
+    // InYear(Box<YearExpr>, i16),
+    // Since(Box<DateExpr>, Duration),
+    // NthSince(Box<DateExpr>, isize, DayOfWeek)
+    InNDays(usize),
+}
+
+#[derive(Debug, PartialEq)]
+enum TimeExpr {
+    Now,
+    Absolute(NaiveTime),
+    InDay(Box<DateExpr>),
+    InNHours(usize),
+    InNMins(usize),
+}
+
+// Recognize
+
+// /// An approximate parsing result:
+// struct Recognized<'a, T> {
+//     value: T,
+//     confidence: f32,
+//     rest: &'a str,
+// }
+
+/// Trait for types that can be parsed.
+trait Recognizable: Sized {
+    fn recognize(text: &str) -> Option<(Self, &str)>;
+
+    fn describe() -> &'static str;
+}
+
+impl Recognizable for YearExpr {
+    fn recognize(text: &str) -> Option<(YearExpr, &str)> {
+        unimplemented!()
+    }
+
+    fn describe() -> &'static str {
+        "month"
+    }
+}
+
+impl Recognizable for MonthExpr {
+    fn recognize(text: &str) -> Option<(MonthExpr, &str)> {
+        unimplemented!()
+    }
+
+    fn describe() -> &'static str {
+        "month"
+    }
+}
+
+impl Recognizable for WeekExpr {
+    fn recognize(text: &str) -> Option<(WeekExpr, &str)> {
+        unimplemented!()
+    }
+
+    fn describe() -> &'static str {
+        "week"
+    }
+}
+
+impl Recognizable for DateExpr {
+    fn recognize(text: &str) -> Option<(DateExpr, &str)> {
+        unimplemented!()
+    }
+
+    fn describe() -> &'static str {
+        "date"
+    }
+}
+
+// https://github.com/wanasit/chrono/blob/master/src/parsers/en/ENTimeExpressionParser.js
+impl Recognizable for TimeExpr {
+    fn recognize(text: &str) -> Option<(TimeExpr, &str)> {
+        unimplemented!()
+    }
+
+    fn describe() -> &'static str {
+        "time of day"
+    }
+}
+
+// Tests
+#[cfg(test)]
+mod time_expr_tests {
+    use super::{Recognizable, TimeExpr};
+    use chrono::NaiveTime;
+
+    #[test]
+    fn simple_hour_tests() {
+        assert_recognize_time("12", 12, 0);
+    }
+
+    #[test]
+    fn am_pm_hour_tests() {
+        assert_recognize_time("10am", 10, 0);
+        assert_recognize_time("10pm", 22, 0);
+    }
+
+    #[test]
+    fn simple_minute_tests() {
+        assert_recognize_time("12:30", 12, 30);
+    }
+
+    #[test]
+    fn am_pm_minute_tests() {
+        assert_recognize_time("10:30am", 10, 30);
+        assert_recognize_time("10pm", 22, 30);
+    }
+
+    fn assert_recognize_time(text: &str, expected_h: u32, expected_m: u32) {
+        // assert_eq!(
+        //     TimeExpr::recognize(text),
+        //     Ok((
+        //         TimeExpr::Absolute(NaiveTime::from_hms(expected_h, expected_m, 0)),
+        //         text
+        //     ))
+        // )
+    }
+}
+
+// Fuzzy Helpers?
+
+// /// Returns true if and only if the text matches the pattern.
+// fn is_match_helper(text: &str, patterns: &[&str]) -> bool {
+//     let re = RegexSet::new(patterns).expect("Invalid Patterns");
+//     re.is_match(text)
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // use std::io;
 // use std::io::prelude::*;
+use chrono::{DateTime, Local, NaiveDate, NaiveDateTime, NaiveTime, Utc};
+use icalendar::{Component, Event};
+// use regex::Regex;
 
 fn main() {
     println!("Hello World");
@@ -9,99 +12,95 @@ fn main() {
     // }
 }
 
-// extern crate chrono;
-// extern crate icalendar;
-// extern crate regex;
+// Examples
+// Starts: Lunch (12pm, 12, noon, twelve, at 12}
+// AllDay: Dillo Day {Saturday, 6/1, sat, this saturday, next saturday, june 1, june 1st}
+// StartsAndEnds: Concert {7-9pm, 7-9, 7 to 9, from 7 to 9, from seven to nine}
+// AllDayStartsAndEnds: Welcome Week {9/1-9/8, September {1st-8, 1-8}}
 
-// use chrono::{NaiveDate, NaiveTime};
-// use icalendar::{Component, Event};
-// // use regex::Regex;
-// use std::iter;
+// Structs
+/// Abstract expression for the start and end of an event.
+enum EventStartAndEndExpr {
+    Starts(NaiveTime),
+    StartsAndEnds(NaiveTime, NaiveTime),
+    StartsWithDate(NaiveTime, NaiveDate),
+    StartsAndEndsWithDate(NaiveTime, NaiveTime, NaiveDate),
+    AllDay(NaiveDate),
+    AllDayStartsAndEnds(NaiveDate, NaiveDate),
+}
 
-// /// Parses input string into Event
-// pub fn parse_input(input: &str) -> Event {
-//     println!("Input: {}", input);
-
-//     let mut e = Event::new();
-
-//     // summary
-//     if let Some(s) = get_summary(input) {
-//         e.summary(&s);
-//     }
-
-//     // location
-//     if let Some(loc) = get_location(input) {
-//         e.location(&loc);
-//     }
-
-//     // start time/date and end time/date
-//     set_start_and_end(&mut e);
-
-//     e.done()
+// struct 'a T> {
+//     result: T,
+//     rest: &'a str,
 // }
 
-// /// Sets the start time/date and end time/date for an `Event`.
-// fn set_start_and_end(e: &mut Event) {
-//     // Time Parsing:
-//     // Looks for a start time and end time
-//     //  if no start time exists, then assume all day/days event
-//     //      look for start date and end date
-//     //          if no start date exists, default to today -> all-day event
-//     //          if start date exists abut no end date, default to 1 day -> all day event
-//     //          else start date and end date event
-//     // if start time exists but no end time, default to 1 hour
+// Parse Function
 
-//     // Assume NOT all day event: look for date
-//     //  if no date exists, default to today
-// }
+/// Parses input string into Event
+pub fn parse_input(text: &str) -> Event {
+    println!("Input: {}", text);
 
-// /// Returns an `Iterator` of all dates found in `input` as `NaiveDate`s.
-// fn get_dates(input: &str) -> impl Iterator<Item = NaiveDate> {
-//     iter::empty::<NaiveDate>()
-// }
+    let mut e = Event::new();
 
-// /// Returns an `Iterator` of all times found in `input` as `NaiveTime`s.
-// pub fn get_times(input: &str) -> impl Iterator<Item = NaiveTime> {
-//     iter::empty::<NaiveTime>()
-//     // let re = Regex::new(r"(\d+:\d+)").unwrap();
-//     // let mat = re.find(input);
+    let now = chrono::Local::now();
+    let today = chrono::Local::today();
 
-//     // if let Some(s) = mat {
-//     //     let mut iter = s.as_str().split(':');
-//     //     let h = iter.next().unwrap().parse().unwrap();
-//     //     let m = iter.next().unwrap().parse().unwrap();
-//     //     return Some(NaiveTime::from_hms(h, m, 0));
-//     // }
-// }
+    // start time/date and end time/date
+    if let Some(exp) = get_start_and_end(text) {
+        use EventStartAndEndExpr::*;
+        match exp {
+            Starts(t) => {
+                // check if parsed time is later than current time, if so, set default day to tomorrow, otherwise, default to today
 
-// /// Returns an `Option` containing an event's summary string parsed from `input`.
-// fn get_summary(input: &str) -> Option<String> {
-//     Some("Example Summary".to_owned())
-// }
+                let ndt = NaiveDateTime::new(today.naive_utc(), t);
+                // e.starts(DateTime::<Local>::from_utc(ndt, Local));
+            } // ...
+            _ => {}
+        }
+    }
 
-// /// Returns an `Option` containing an event location string parsed from `input`.
-// fn get_location(input: &str) -> Option<String> {
-//     Some("Example Location".to_owned())
-// }
+    // location
+    if let Some(loc) = get_location(text) {
+        e.location(&loc);
+    }
 
-// mod test {
-//     // use super::get_times;
-//     // use chrono::NaiveTime;
+    // summary
+    if let Some(summary) = get_summary(text) {
+        e.summary(&summary);
+    }
 
-//     // #[test]
-//     // fn colon_time_test() {
-//     //     assert_time("Breakfast on Friday at 10:30", 10, 30);
-//     // }
+    e.done()
+}
 
-//     // #[test]
-//     // fn am_pm_time_test() {
-//     //     assert_time("Lunch on Friday at 12pm", 12, 0);
-//     // }
+/// Returns an `Option` containing an `EventStartAndEndExpr`.
+fn get_start_and_end(input: &str) -> Option<EventStartAndEndExpr> {
+    unimplemented!()
+    // Hack: look for {'-', "to"}, if found, then it's a StartsAndEnds, StartsAndEndsWithDate, or AllDayStartsAndEnds
+    //  Get expressions before and after {'-', "to"}
+    // else parse input for Start
+    //  Hack: search for {12am, midnight}, if not found, if 12am is returned, it's an AllDay, or AllDayStartsAndEnds
+    //  else it's a Starts
+    // if not found, default to now
 
-//     // fn assert_time(input: &str, expected_h: u32, expected_m: u32) {
-//     //     assert_eq!(
-//     //         get_times(&input),
-//     //         Some(NaiveTime::from_hms(expected_h, expected_m, 0))
-//     //     )
-//     // }
-// }
+    // Previous Time Parsing:
+    // Looks for a start time and end time
+    //  if no start time exists, then assume all day/days event
+    //      look for start date and end date
+    //          if no start date exists, default to today -> all-day event
+    //          if start date exists abut no end date, default to 1 day -> all day event
+    //          else start date and end date event
+    // if start time exists but no end time, default to 1 hour
+
+    // Assume NOT all day event: look for date
+    //  if no date exists, default to today
+}
+
+/// Returns an `Option` containing an event's summary string parsed from `input`.
+fn get_summary(input: &str) -> Option<String> {
+    Some("Example Summary".to_owned())
+}
+
+/// Returns an `Option` containing an event location string parsed from `input`.
+fn get_location(input: &str) -> Option<String> {
+    Some("Example Location".to_owned())
+}
